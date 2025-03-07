09efd86d704c7044fddbc88305ff7f6fc33de1ba
supu345 <supu345@gmail.com>
2025-03-07 11:51:21 +0600

    api

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 const api = axios.create({
-  //baseURL: "https://car-parts-mern-api.onrender.com",
+  baseURL: "https://car-parts-mern-api.onrender.com",
 
-  baseURL: "http://localhost:5000",
+  //baseURL: "http://localhost:5000",
   withCredentials: true,
 });
 export default api;
